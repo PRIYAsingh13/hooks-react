495a47a9af5f0dd0315874958621d84f88173725
PRIYAsingh13 <ps597934@gmail.com>
2023-02-16 21:01:19 +0530

    element

--- src/App.js
+++ src/App.js
@@ -2,25 +2,31 @@ import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const state = useState();
-  const [count, setCount] = useState(0);
 
-  const Increment = () => {
-    setCount(count + 1);
-  };
-  const Decrement = () => {
-    setCount(count - 1);
-  };
+  // const  List = ()=> React.createElement('div',null,'priya singh')
+
+  const List = () => {
+    return(
+      <div>priya</div>
+    )
+  }
+  // const state = useState();
+  // const [count, setCount] = useState(0);
+
+  // const Increment = () => {
+  //   setCount(count + 1);
+  // };
+  // const Decrement = () => {
+  //   setCount(count - 1);
+  // };
 
 
   return (
-    <>
+    
       <div>
-        <h1> {count} </h1>
-        <button onClick={Increment}> Increment</button>
-        <button onClick={Decrement}> Decrement</button>
+       <List/>
       </div>
-    </>
+    
   );
 };
 
